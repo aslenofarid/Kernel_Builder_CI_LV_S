c2b2327bf6651b8431941a074ab98294507eae95
aslenofarid <yoniasleno.farid14@gmail.com>
2024-08-03 22:28:03 +0000

    r42

--- build.sh
+++ build.sh
@@ -48,13 +48,13 @@ DEVICE="X00TD"
 
 # The defconfig which should be used. Get it from config.gz from
 # your device or check source
-DEFCONFIG=potum_defconfig
+DEFCONFIG=X00TD_defconfig
 
 # Show manufacturer info
 MANUFACTURERINFO="ASUSTek Computer Inc."
 
 # Kernel Variant
-NAMA=potum-ksu
+NAMA=kernel.lnx.4.4.r42-rel
 
 KERNEL_FOR=NLV
 
@@ -177,7 +177,7 @@ DATE2=$(TZ=Asia/Jakarta date +"%Y%m%d")
 # Function to replace defconfig versioning
 setversioning() {
     # For staging branch
-    KERNELNAME="$JENIS-$NAMA"
+    KERNELNAME="$JENIS-$NAMA$VARIAN"
     # Export our new localversion and zipnames
     export KERNELNAME
     export ZIPNAME="$KERNELNAME.zip"
